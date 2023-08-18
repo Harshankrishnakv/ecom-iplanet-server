c2aa206a1e441056f04d4ffe9edcbcce54e3f955
Harshankrishna <140307793+Harshankrishnakv@users.noreply.github.com>
2023-08-18 18:24:24 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -30,7 +30,7 @@ const UserSchema = mongoose.Schema(
       type: Array,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-      required: [true, "is required"],
+      
     },
 
     isAdmin: {
